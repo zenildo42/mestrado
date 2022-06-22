f153ea1a911192b1d151db85a2671bc26f8dd7d6
zenildo42 <zenildo@gmail.com>
2022-06-22 11:14:48 -0300

    tx04

--- projects/ze-tx/src/main.cpp
+++ projects/ze-tx/src/main.cpp
@@ -141,7 +141,7 @@ static void prvTransmitTask(void *pvParameters)
 
   /* Get EUI48 address */
   /* board.getEUI48(eui48_address);*/
-  eui48_address = '\\x00\\x12K\\xe0\\xba\\x01';
+  eui48_address = "\\x00\\x12K\\xe0\\xba\\x01";
 
   /* Initialize BME280 sensors */
   // bme280.init();
