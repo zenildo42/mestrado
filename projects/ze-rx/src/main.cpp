b7338b139d385e90600a597bc5b4ff051542c149
zenildo42 <zenildo@gmail.com>
2022-06-23 10:22:43 -0300

    RX OQPSK

--- projects/ze-rx/src/main.cpp
+++ projects/ze-rx/src/main.cpp
@@ -43,12 +43,12 @@
 // #define RADIO_FREQUENCY (&frequency_settings_09[FREQUENCY_09_OFDM2]) /* OFDM Mode 2,  800 kHz */
 
 /* FSK Radio settings - USB03*/
-#define RADIO_SETTINGS (&radio_settings[CONFIG_FSK_OPTION1])        /* X 2-FSK,  50 kbps */
-#define RADIO_FREQUENCY (&frequency_settings_09[FREQUENCY_09_FSK1]) /* FSK Mode 1,   200 kHz */
+// #define RADIO_SETTINGS (&radio_settings[CONFIG_FSK_OPTION1])        /* X 2-FSK,  50 kbps */
+// #define RADIO_FREQUENCY (&frequency_settings_09[FREQUENCY_09_FSK1]) /* FSK Mode 1,   200 kHz */
 
 /* OQPSK Radio settings - USB05*/
-// #define RADIO_SETTINGS (&radio_settings[CONFIG_OQPSK_RATE4])         /* X OQPSK-DSSS,  100 kchips/s,  50.00 kbps */
-// #define RADIO_FREQUENCY (&frequency_settings_09[FREQUENCY_09_OQPSK]) /* OQPSK,        600 kHz */
+#define RADIO_SETTINGS (&radio_settings[CONFIG_OQPSK_RATE4])         /* X OQPSK-DSSS,  100 kchips/s,  50.00 kbps */
+#define RADIO_FREQUENCY (&frequency_settings_09[FREQUENCY_09_OQPSK]) /* OQPSK,        600 kHz */
 
 #define RADIO_CHANNEL (0)
 #define RADIO_TX_POWER (At86rf215::TransmitPower::TX_POWER_MIN)
